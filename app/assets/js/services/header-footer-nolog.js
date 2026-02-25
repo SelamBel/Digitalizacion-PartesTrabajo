81627a3ca2753fcb1c8acaf8864c7ade75410885
Selam Bel <116879445+SelamBel@users.noreply.github.com>
2026-02-25 10:44:11 +0100

    Dando formato automático a los archivos

--- app/assets/js/services/header-footer-nolog.js
+++ app/assets/js/services/header-footer-nolog.js
@@ -1,20 +1,20 @@
 import { auth } from "../firebase/firebase-config.js";
 import { signOut } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
-$(document).ready(function() {
+$(document).ready(function () {
 
     initTheme();
 
-    $(".header-container").load("assets/header-footer/header-nolog.html", function() {
+    $(".header-container").load("assets/header-footer/header-nolog.html", function () {
         $("#headerTitle").text(document.title);
 
         const temaActual = localStorage.getItem("tema") || "default";
-        const btnActivo = $(".btnCambiarTema").filter(function() {
+        const btnActivo = $(".btnCambiarTema").filter(function () {
             return $(this).text().toLowerCase() === temaActual;
         });
         btnActivo.closest("ul").prependTo("#liBtnCambiarTema");
 
-        $(".btnCambiarTema").on("click", function() {
+        $(".btnCambiarTema").on("click", function () {
             const tema = $(this).data("tema") || $(this).text().toLowerCase();
             setTheme(tema);
             $(this).closest("ul").prependTo("#liBtnCambiarTema");
--- app/assets/js/services/header-footer.js
+++ app/assets/js/services/header-footer.js
@@ -1,26 +1,26 @@
 import { auth } from "../firebase/firebase-config.js";
 import { signOut } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
-$(document).ready(function() {
+$(document).ready(function () {
 
     initTheme();
 
-    $(".header-container").load("assets/header-footer/header.html", function() {
+    $(".header-container").load("assets/header-footer/header.html", function () {
         $("#headerTitle").text(document.title);
 
         const temaActual = localStorage.getItem("tema") || "default";
-        const btnActivo = $(".btnCambiarTema").filter(function() {
+        const btnActivo = $(".btnCambiarTema").filter(function () {
             return $(this).text().toLowerCase() === temaActual;
         });
         btnActivo.closest("ul").prependTo("#liBtnCambiarTema");
 
-        $(".btnCambiarTema").on("click", function() {
+        $(".btnCambiarTema").on("click", function () {
             const tema = $(this).data("tema") || $(this).text().toLowerCase();
             setTheme(tema);
             $(this).closest("ul").prependTo("#liBtnCambiarTema");
         });
 
-        $("#btnCerrarSesion").on("click", async function() {
+        $("#btnCerrarSesion").on("click", async function () {
             try {
                 await signOut(auth);
                 console.log("Sesión cerrada correctamente");
